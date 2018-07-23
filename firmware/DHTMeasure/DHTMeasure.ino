ad9790aa5a47f17e2467ddcfe66bc33a39d3b0db
Vladimir Boris <vladimir.boris.bel@gmail.com>
2018-07-22 23:18:17 -0600

    firmware refactoring

--- firmware/DHTMeasure/DHTMeasure.ino
+++ firmware/DHTMeasure/DHTMeasure.ino
@@ -1,16 +1,15 @@
-/*
- *  This sketch sends data via HTTP GET requests to data.sparkfun.com service.
- *
- *  You need to get streamId and privateKey at data.sparkfun.com and paste them
- *  below. Or just customize this script to talk to other HTTP servers.
- *
- */ 
 #include "DHT.h"
 #include <ESP8266WiFi.h>
 
+// DHT
 #define DHTPIN D1
 #define DHTTYPE DHT11
+#define DHT_TIMEOUT 2000
+
+// Wifi
 #define PORT 3000
+#define WIFIPIN D2
+#define WIFI_TIMEOUT 5000
 
 DHT dht(DHTPIN, DHTTYPE);
 
@@ -26,22 +25,30 @@ struct measure {
 
 measure msr;
 
-// WiFi Connection delay Counter
-unsigned int connVal = 0;
-// Measure delay Counter
-unsigned int msrVal = 0;
+// WiFi Connection timeStapm
+unsigned long conn_timeStamp = -1;
+// Measure timeStamp
+unsigned int msr_timeStamp = -1;
 
 bool readDht(measure *msr) {
   float humi = dht.readHumidity();
   float cels = dht.readTemperature();
   float fahr = dht.readTemperature(true);
   
-  /*if(isnan(humi) || isnan(cels) || isnan(fahr))
-    return false;*/
-  
   msr->h = humi;
   msr->t = cels;
   msr->f = fahr;
+
+  Serial.print("\n\t\n--------------------MEASURE------------------\n");
+  Serial.print("Humidity: ");
+  Serial.print(msr->h);
+  Serial.print(" %\t");
+  Serial.print("Temperature: ");
+  Serial.print(msr->t);
+  Serial.print(" *C ");
+  Serial.print(msr->f);
+  Serial.print(" *F");
+  Serial.print("\n---------------------------------------------\t\n\n");
   
   return true;
 }
@@ -57,17 +64,9 @@ bool conn() {
     Serial.println("connection failed");
     return false;
   }
-  
-  // We now create a URI for the request
+
+  // URL for request
   String url = "/";
- /* 
-  url += "?humi=";
-  url += msr.h;
-  url += "&cels=";
-  url += msr.t;
-  url += "&fahr=";
-  url += msr.f; 
- */
 
   // POST data
   String data = "humi=" + String(msr.h) + 
@@ -77,8 +76,8 @@ bool conn() {
   Serial.print("Requesting URL: ");
   Serial.println(url);
 
-  digitalWrite(D2, HIGH);
-  // This will send the request to the server
+  digitalWrite(WIFIPIN, HIGH);
+  // Sending the request to the server
   client.print(String("POST ") + url + " HTTP/1.1\r\n" +
                "Host: " + host + "\r\n" + 
                "Accept: */*\r\n" +
@@ -90,19 +89,17 @@ bool conn() {
                            
   unsigned long timeout = millis();
   while (client.available() == 0) {
-    if (millis() - timeout > 5000) {
-      Serial.println(">>> Client Timeout !");
-      client.stop();
-      digitalWrite(D2, LOW);
-      return true;
-    }
-    digitalWrite(D2, LOW);
-  }
-  
-  // Read all the lines of the reply from server and print them to Serial
-  while(client.available()) {
+    // Read all the lines of the reply from server and print them to Serial
     String line = client.readStringUntil('\r');
     Serial.print(line);
+    
+    if (millis() - timeout > WIFI_TIMEOUT) {
+      Serial.println(">>> Client Timeout !");
+      client.stop();
+      digitalWrite(WIFIPIN, LOW);
+      return true;
+    }
+    digitalWrite(WIFIPIN, LOW);
   }
   
   Serial.println();
@@ -110,16 +107,15 @@ bool conn() {
 
   return true;
 }
-  
+
 void setup() {
   Serial.begin(9600);
   dht.begin();
   delay(10);
 
-  pinMode(D2, OUTPUT);
-  digitalWrite(D2, LOW);
-
-  // We start by connecting to a WiFi network
+  // Wifi indicator
+  pinMode(WIFIPIN, OUTPUT);
+  digitalWrite(WIFIPIN, LOW);
 
   Serial.println();
   Serial.println();
@@ -140,30 +136,24 @@ void setup() {
 }
 
 void loop() {
-  if(msrVal++ == 2000) {
+  if(msr_timeStamp == -1)
+    msr_timeStamp = millis();
+  
+  if (millis() - msr_timeStamp > DHT_TIMEOUT) {
+    msr_timeStamp = -1;
+    
     if(!readDht(&msr))
-      Serial.println("Measure was falled");
-
-    Serial.print("\n\t\n--------------------MEASURE------------------\n");
-    Serial.print("Humidity: ");
-    Serial.print(msr.h);
-    Serial.print(" %\t");
-    Serial.print("Temperature: ");
-    Serial.print(msr.t);
-    Serial.print(" *C ");
-    Serial.print(msr.f);
-    Serial.print(" *F");
-    Serial.print("\n---------------------------------------------\t\n\n");
-      
-    msrVal = 0;  
+      Serial.println("Measure was falled");  
   }
 
-  connVal++;
-  if(connVal == 5000) {
+  if(conn_timeStamp == -1)
+    conn_timeStamp = millis();
+    
+  if (millis() - conn_timeStamp > WIFI_TIMEOUT) {
+    conn_timeStamp = -1;
+    
     if(!conn())
       Serial.println("Connection was falled");
-    
-    connVal = 0;
   }
 }
 
